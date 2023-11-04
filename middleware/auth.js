41e49d16f96fcdb1ff9da444076af9d381d9d8c3
Mike Downes <mikedownes.dev@gmail.com>
2023-11-04 15:42:27 -0400

    auth-handling middleware

---
+++ middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = async (req, res, next) => {
+    // Get token from the headers
+    const token = req.header('authorization');
+
+    // Check if token exists
+    if (!token) {
+        return res.status(401).json({ msg: 'No token, authorization denied' });
+    }
+
+    try {
+        // Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Set user in request object
+        req.user = decoded.user;
+
+        next();
+    } catch (err) {
+        console.error(err.message);
+        res.status(401).json({ msg: 'Token is not valid' });
+    }
+};
+
+module.exports = authMiddleware;
