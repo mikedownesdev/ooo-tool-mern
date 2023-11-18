28097860ce1bce6e6b9f3c293935344355fa12a7
Mike Downes <mikedownes.dev@gmail.com>
2023-11-18 15:54:08 -0500

    some misc cleanup before first deploy

--- models/Employee.js
+++ models/Employee.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 const employeeSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    name: String,
-    team: { type: mongoose.Schema.Types.ObjectId, ref: 'Team', required: true },
-    salaried: { type: Boolean, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    team: { type: mongoose.Schema.Types.ObjectId, ref: 'Team', required: false },
+    salaried: { type: Boolean, required: false },
 
     // System Fields
     createdAt: { type: Date, default: Date.now },
--- routes/requests.js
+++ routes/requests.js
@@ -1,35 +1,35 @@
 const express = require('express');
 const router = express.Router();
-const timeoffController = require('../controllers/requestController');
+const requestController = require('../controllers/requestController');
 
 // Create a Time Off Request
-router.post('/requests', timeoffController.createTimeOffRequest);
+router.post('/requests', requestController.createTimeOffRequest);
 
 // List All Time Off Requests
-router.get('/requests', timeoffController.getAllTimeOffRequests);
+router.get('/requests', requestController.getAllTimeOffRequests);
 
 // Get a Specific Time Off Request
-router.get('/requests/:id', timeoffController.getSingleTimeOffRequest);
+router.get('/requests/:id', requestController.getSingleTimeOffRequest);
 
 // Update a Time Off Request
-router.put('/-requests/:id', timeoffController.updateTimeOffRequest);
+router.put('/requests/:id', requestController.updateTimeOffRequest);
 
 // Delete a Time Off Request
-router.delete('/requests/:id', timeoffController.deleteTimeOffRequest);
+router.delete('/requests/:id', requestController.deleteTimeOffRequest);
 
 // Approve a Time Off Request (for managers)
-router.put('/requests/:id/approve', timeoffController.approveTimeOffRequest);
+router.put('/requests/:id/approve', requestController.approveTimeOffRequest);
 
 // Reject a Time Off Request (for managers)
-router.put('/requests/:id/reject', timeoffController.rejectTimeOffRequest);
+router.put('/requests/:id/reject', requestController.rejectTimeOffRequest);
 
 // List Time Off Requests for a Specific Employee
-router.get('/employees/:employeeId/requests', timeoffController.getTimeOffRequestsForEmployee);
+router.get('/employees/:employeeId/requests', requestController.getTimeOffRequestsForEmployee);
 
 // List Time Off Requests by Status
-router.get('/requests', timeoffController.getTimeOffRequestsByStatus);
+router.get('/requests', requestController.getTimeOffRequestsByStatus);
 
 // Filter Time Off Requests by Date Range
-router.get('/requests', timeoffController.getTimeOffRequestsByDateRange);
+router.get('/requests', requestController.getTimeOffRequestsByDateRange);
 
 module.exports = router;
